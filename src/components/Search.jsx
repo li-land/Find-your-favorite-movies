5d9749c7e07786f00eca5c62f2d3f75a1e447149
Ildar <ildarGatiya@gmail.com>
2022-02-21 11:18:04 +0500

    change class to function components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,90 +1,175 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: "",
-      type: "",
-    };
-  }
+const Search = (props) => {
+  const { searchMovies = Function.prototype } = props;
 
-  handleSearchValue = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
+  const [search, setSearch] = useState("");
+  const [type, setType] = useState("");
+
+  const handleSearchValue = (event) => {
+    event.target.name === "search"
+      ? setSearch(event.target.value)
+      : setType(event.target.value);
   };
-  handleKey = (event) => {
+
+  const handleKey = (event) => {
     if (event.key === "Enter") {
-      this.props.searchMovies(this.state.search, this.state.type);
+      searchMovies(search, type);
     }
   };
-  render() {
-    const { search, type } = this.state;
-    const { searchMovies } = this.props;
 
-    return (
-      <div className="page__search search">
-        <div className="search__form">
-          <div className="search__wrapp">
-            <input
-              className="search__field"
-              type="search"
-              name="search"
-              placeholder="Write the name of the movie"
-              value={search}
-              onChange={this.handleSearchValue}
-              onKeyDown={this.handleKey}
-            />
-            <button
-              className="search__btn"
-              onClick={() => searchMovies(search, type)}
-            >
-              Search
-            </button>
+  return (
+    <div className="page__search search">
+      <div className="search__form">
+        <div className="search__wrapp">
+          <input
+            className="search__field"
+            type="search"
+            name="search"
+            placeholder="Write the name of the movie"
+            value={search}
+            onChange={handleSearchValue}
+            onKeyDown={handleKey}
+          />
+          <button
+            className="search__btn"
+            onClick={() => searchMovies(search, type)}
+          >
+            Search
+          </button>
+        </div>
+        <div className="search__radio-box">
+          <div className="search__radio-item">
+            <label htmlFor="radio-all">
+              <input
+                id="radio-all"
+                type="radio"
+                name="type"
+                value=""
+                checked={type === ""}
+                onChange={handleSearchValue}
+              />
+              All
+            </label>
           </div>
-          <div className="search__radio-box">
-            <div className="search__radio-item">
-              <label htmlFor="radio-all">
-                <input
-                  id="radio-all"
-                  type="radio"
-                  name="type"
-                  value=""
-                  checked={type === ""}
-                  onChange={this.handleSearchValue}
-                />{" "}
-                All
-              </label>
-            </div>
-            <div className="search__radio-item">
-              <label htmlFor="radio-movie">
-                <input
-                  id="radio-movie"
-                  type="radio"
-                  name="type"
-                  value="movie"
-                  checked={type === "movie"}
-                  onChange={this.handleSearchValue}
-                />{" "}
-                Only movies
-              </label>
-            </div>
-            <div className="search__radio-item">
-              <label htmlFor="radio-series">
-                <input
-                  id="radio-series"
-                  type="radio"
-                  name="type"
-                  value="series"
-                  checked={type === "series"}
-                  onChange={this.handleSearchValue}
-                />{" "}
-                Only series
-              </label>
-            </div>
+          <div className="search__radio-item">
+            <label htmlFor="radio-movie">
+              <input
+                id="radio-movie"
+                type="radio"
+                name="type"
+                value="movie"
+                checked={type === "movie"}
+                onChange={handleSearchValue}
+              />
+              Only movies
+            </label>
+          </div>
+          <div className="search__radio-item">
+            <label htmlFor="radio-series">
+              <input
+                id="radio-series"
+                type="radio"
+                name="type"
+                value="series"
+                checked={type === "series"}
+                onChange={handleSearchValue}
+              />
+              Only series
+            </label>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 export { Search };
+
+// class Search extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       search: "",
+//       type: "",
+//     };
+//   }
+
+//   handleSearchValue = (event) => {
+//     this.setState({ [event.target.name]: event.target.value });
+//   };
+//   handleKey = (event) => {
+//     if (event.key === "Enter") {
+//       this.props.searchMovies(this.state.search, this.state.type);
+//     }
+//   };
+//   render() {
+//     const { search, type } = this.state;
+//     const { searchMovies } = this.props;
+
+//     return (
+//       <div className="page__search search">
+//         <div className="search__form">
+//           <div className="search__wrapp">
+//             <input
+//               className="search__field"
+//               type="search"
+//               name="search"
+//               placeholder="Write the name of the movie"
+//               value={search}
+//               onChange={this.handleSearchValue}
+//               onKeyDown={this.handleKey}
+//             />
+//             <button
+//               className="search__btn"
+//               onClick={() => searchMovies(search, type)}
+//             >
+//               Search
+//             </button>
+//           </div>
+//           <div className="search__radio-box">
+//             <div className="search__radio-item">
+//               <label htmlFor="radio-all">
+//                 <input
+//                   id="radio-all"
+//                   type="radio"
+//                   name="type"
+//                   value=""
+//                   checked={type === ""}
+//                   onChange={this.handleSearchValue}
+//                 />{" "}
+//                 All
+//               </label>
+//             </div>
+//             <div className="search__radio-item">
+//               <label htmlFor="radio-movie">
+//                 <input
+//                   id="radio-movie"
+//                   type="radio"
+//                   name="type"
+//                   value="movie"
+//                   checked={type === "movie"}
+//                   onChange={this.handleSearchValue}
+//                 />{" "}
+//                 Only movies
+//               </label>
+//             </div>
+//             <div className="search__radio-item">
+//               <label htmlFor="radio-series">
+//                 <input
+//                   id="radio-series"
+//                   type="radio"
+//                   name="type"
+//                   value="series"
+//                   checked={type === "series"}
+//                   onChange={this.handleSearchValue}
+//                 />{" "}
+//                 Only series
+//               </label>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export { Search };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,20 +1,53 @@
-import React, { useLayoutEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Movies } from "../components/Movies";
 import { Preloader } from "../components/Preloader";
 import { Search } from "../components/Search";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-class Main extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      movies: [],
-      loading: true,
-    };
-  }
+function Main() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  componentDidMount() {
+  const searchMovies = (search, type) => {
+    setLoading(true);
+    let arr = [];
+    let searchMovie = search.toLowerCase();
+    fetch(
+      `https://www.omdbapi.com/?s=${searchMovie}&type=${type}&apikey=${API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.Search) {
+          setMovies(arr);
+          setLoading(false);
+        } else {
+          data.Search.forEach((data) => {
+            fetch(`https://www.omdbapi.com/?i=${data.imdbID}&apikey=${API_KEY}`)
+              .then((response) => response.json())
+              .then((info) => {
+                arr.push({
+                  Poster: info.Poster,
+                  Title: info.Title,
+                  Year: info.Year,
+                  Plot: info.Plot,
+                  Type: info.Type,
+                  imdbID: info.imdbID,
+                });
+                if (arr.length === 10) {
+                  setMovies(arr);
+                  setLoading(false);
+                }
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          });
+        }
+      });
+  };
+
+  useEffect(() => {
     let arr = [];
     fetch(`https://www.omdbapi.com/?s=spider man&apikey=${API_KEY}`)
       .then((response) => response.json())
@@ -31,54 +64,104 @@ class Main extends React.Component {
                 Type: info.Type,
                 imdbID: info.imdbID,
               });
-              this.setState({ movies: arr, loading: false });
+              if (arr.length === 10) {
+                setMovies(arr);
+                setLoading(false);
+              }
+            })
+            .catch((error) => {
+              console.log(error);
             });
         });
       });
-  }
+  }, []);
 
-  searchMovies = (search, type) => {
-    this.setState({ loading: true });
-    let arr = [];
-    const searchMovie = search.toLowerCase();
-    fetch(
-      `https://www.omdbapi.com/?s=${searchMovie}&type=${type}&apikey=${API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.Search) {
-          this.setState({ movies: arr, loading: false });
-        } else {
-          data.Search.forEach((data) => {
-            fetch(`https://www.omdbapi.com/?i=${data.imdbID}&apikey=${API_KEY}`)
-              .then((response) => response.json())
-              .then((info) => {
-                arr.push({
-                  Poster: info.Poster,
-                  Title: info.Title,
-                  Year: info.Year,
-                  Plot: info.Plot,
-                  Type: info.Type,
-                  imdbID: info.imdbID,
-                });
-                this.setState({ movies: arr, loading: false });
-              });
-          });
-        }
-      });
-  };
-
-  render() {
-    const { movies, loading } = this.state;
-    return (
-      <main className="page">
-        <div className="page__container container">
-          <Search searchMovies={this.searchMovies} />
-          {loading ? <Preloader /> : <Movies movies={movies} />}
-        </div>
-      </main>
-    );
-  }
+  return (
+    <main className="page">
+      <div className="page__container container">
+        <Search searchMovies={searchMovies} />
+        {loading ? <Preloader /> : <Movies movies={movies} />}
+      </div>
+    </main>
+  );
 }
 
 export { Main };
+
+// class Main extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       movies: [],
+//       loading: true,
+//     };
+//   }
+
+//   componentDidMount() {
+//     let arr = [];
+//     fetch(`https://www.omdbapi.com/?s=spider man&apikey=${API_KEY}`)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         data.Search.forEach((data) => {
+//           fetch(`https://www.omdbapi.com/?i=${data.imdbID}&apikey=${API_KEY}`)
+//             .then((response) => response.json())
+//             .then((info) => {
+//               arr.push({
+//                 Poster: info.Poster,
+//                 Title: info.Title,
+//                 Year: info.Year,
+//                 Plot: info.Plot,
+//                 Type: info.Type,
+//                 imdbID: info.imdbID,
+//               });
+//               this.setState({ movies: arr, loading: false });
+//             });
+//         });
+//       });
+//   }
+
+//   searchMovies = (search, type) => {
+//     this.setState({ loading: true });
+//     let arr = [];
+//     const searchMovie = search.toLowerCase();
+//     fetch(
+//       `https://www.omdbapi.com/?s=${searchMovie}&type=${type}&apikey=${API_KEY}`
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         if (!data.Search) {
+//           this.setState({ movies: arr, loading: false });
+//         } else {
+//           data.Search.forEach((data) => {
+//             fetch(`https://www.omdbapi.com/?i=${data.imdbID}&apikey=${API_KEY}`)
+//               .then((response) => response.json())
+//               .then((info) => {
+//                 arr.push({
+//                   Poster: info.Poster,
+//                   Title: info.Title,
+//                   Year: info.Year,
+//                   Plot: info.Plot,
+//                   Type: info.Type,
+//                   imdbID: info.imdbID,
+//                 });
+//                 this.setState({ movies: arr, loading: false });
+//               });
+//           });
+//         }
+//       });
+//   };
+
+//   render() {
+//     const { movies, loading } = this.state;
+//     return (
+//       <main className="page">
+//         <div className="page__container container">
+//           <Search searchMovies={this.searchMovies} />
+//           {loading ? <Preloader /> : <Movies movies={movies} />}
+//         </div>
+//       </main>
+//     );
+//   }
+// }
+
+// export { Main };
